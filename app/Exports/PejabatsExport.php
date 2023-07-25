98efb8b1398d6eae6347d660cbcb2729312d33d0
Thirsya <thirsya.widya@gmail.com>
2023-07-25 11:34:39 +0700

    admin - export pejabat rev

--- app/Exports/PejabatsExport.php
+++ app/Exports/PejabatsExport.php
@@ -11,7 +11,7 @@ class PejabatsExport implements FromCollection,  WithHeadings, ShouldAutoSize
 {
     public function collection()
     {
-        return Pejabat::select('pejabats.nama_pejabat', 'jabatans.jabatan',  'opds.nama_opd', 'opds.no_opd', 'pejabats.nip_pejabat', 'pejabats.no_sk')
+        return Pejabat::select('pejabats.nama_pejabat', 'jabatans.jabatan',  'opds.nama_opd', 'pejabats.nip_pejabat', 'pejabats.no_sk')
             ->join('jabatans', 'pejabats.id_jabatan', '=', 'jabatans.id')
             ->join('opds', 'pejabats.id_opd', '=', 'opds.id')
             ->get();
