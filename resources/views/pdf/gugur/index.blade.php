e62cecf2022314ccf40a23e5e86cbffeee5c79dd
Thirsya <thirsya.widya@gmail.com>
2023-09-29 11:23:21 +0700

    tampilan cetak gugur

---
+++ resources/views/pdf/gugur/index.blade.php
@@ -0,0 +1,49 @@
+<style type="text/css">
+    table, tbody, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        font-size: 10px;
+        font-family: "Courier";
+        font-weight: bold;
+        margin: 0 auto; /* Tambahkan ini untuk mengatur margin otomatis */
+    }
+
+    th {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        font-family: 'Arial';
+        text-align: center;
+    }
+
+    .ktbl {
+        border-left: 2px solid black;
+    }
+
+    .btbl {
+        border-bottom: 2px solid black;
+    }
+</style>
+
+<center style="font-family:'Arial';font-size: 15px;"><b>PEMENANG LELANG SEWA TANAH PERTANIAN YANG GUGUR</b><br> {{--  nama kelurahan --}}
+Masa Sewa <br> {{--  periode tabel daerah --}}
+
+<table>
+<thead>
+    <tr class="btbl">
+        <th>NO</th>
+        <th colspan="2">BUKTI | BIDANG</th>
+        <th>LETAK</th>
+        <th>LUAS</th>
+        <th>HARGA DASAR</th>
+        <th>NAMA PENAWAR</th>
+        <th>NILAI</th>
+        <th>KETERANGAN</th>
+    </tr>
+</thead>
+
+</table>
+</center>
