445586a5079a76d0a1941186309b292e09104958
Thirsya <thirsya.widya@gmail.com>
2023-10-04 11:08:33 +0700

    tampilan lahan tidak laku

---
+++ resources/views/lelang/penawaran/tidak-laku.blade.php
@@ -0,0 +1,54 @@
+<style>
+    .tepi {
+      width:100%;
+      height:auto;
+      border: 2px ridge black;
+    }
+ </style>
+
+<center>
+    <p style="font-size: 9px">Aplikasi Lelang TKD BPPKAD Kota Kediri</p>
+    <div class="tepi"></div>
+    <br>
+    <center style="font-weight: bold">
+        TANAH PERTANIAN BEKAS KAS DESA<br>
+        YANG TIDAK TERPILIH PADA LELANG
+    </center> <br>
+    <table style="float: right">
+        <tr>
+           <td>Lampiran 2 Berita Acara </td>
+       </tr>
+    </table>
+    <br><br><br>
+    <table style="float: left">
+         <tr>
+            <td>Kel. ....</td>
+        </tr>
+    </table>
+    <br><br>
+    <table border="1" style="width:95%;border-color:black;">
+        <tr style="font-size: 14px; font-weight: normal;">
+            <th rowspan="2" >No</th>
+            <th rowspan="2" >Bukti</th>
+            <th rowspan="2" >Alamat</th>
+            <th rowspan="2" >Harga Dasar</th>
+            <th colspan="2" >Obyek</th>
+        </tr>
+        <tr>
+            <th>Padi</th>
+            <th>Kacang</th>
+        </tr>
+    </table>
+    <br><br>
+    <table style="float: right">
+        <tr>
+           <td>Selaku </td>
+       </tr>
+       <tr>
+           <td>Pengguna Barang </td>
+       </tr>
+       <tr>
+           <td>Milik Pemerintah Kota Kediri </td>
+       </tr>
+    </table>
+</center>
