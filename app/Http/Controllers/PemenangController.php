09d01e39a4af7cbefee5580f689934ea0a80a42a
Thirsya <thirsya.widya@gmail.com>
2023-10-12 09:55:34 +0700

    cetak pemenang 1-5

--- app/Http/Controllers/PemenangController.php
+++ app/Http/Controllers/PemenangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Daerah;
+use App\Models\Daftar;
 use App\Models\Penawaran;
 use PDF;
 use Illuminate\Support\Facades\DB;
@@ -23,12 +24,210 @@ class PemenangController extends Controller
             ->first();
 
         $penawaranId = session('penawaran_id');
-        $pemenangs = Penawaran::all();
 
+        $daftarIdFromSession = (int) session('selected_kelurahan_id');
+        $kelurahanIdFromDaerah = Daerah::where('id', $daftarIdFromSession)->pluck('id_kelurahan')->first();
+        $sub = Penawaran::select('idfk_tkd', DB::raw('MAX(nilai_penawaran) as max_penawaran'))
+            ->whereNull('deleted_at')
+            ->where('gugur', '=', false)
+            ->groupBy('idfk_tkd');
+        $penawaran = Penawaran::select(
+            'daftars.no_urut',
+            'daftars.nama',
+            'daftar2.nama as nama2',
+            'daftar3.nama as nama3',
+            'daftar4.nama as nama4',
+            'daftar5.nama as nama5',
+            'daftars.tgl_perjanjian',
+            'tkds.bukti',
+            'tkds.letak',
+            'tkds.bidang',
+            'tkds.harga_dasar',
+            'tkds.luas',
+            'tkds.keterangan',
+            'penawarans.id',
+            'penawarans.total_luas',
+            'penawarans.idfk_tkd',
+            'penawarans.idfk_daftar',
+            'penawarans.nilai_penawaran',
+            DB::raw('
+                    (SELECT nilai_penawaran
+                     FROM penawarans AS subquery
+                     WHERE subquery.idfk_tkd = penawarans.idfk_tkd
+                     AND subquery.nilai_penawaran IS NOT NULL
+                     ORDER BY subquery.nilai_penawaran DESC
+                     LIMIT 1 OFFSET 1) AS nilai_penawaran2
+                '),
+            DB::raw('
+    (SELECT nilai_penawaran
+     FROM penawarans AS subquery
+     WHERE subquery.idfk_tkd = penawarans.idfk_tkd
+     AND subquery.nilai_penawaran IS NOT NULL
+     ORDER BY subquery.nilai_penawaran DESC
+     LIMIT 1 OFFSET 2) AS nilai_penawaran3
+'),
+
+            DB::raw('
+    (SELECT nilai_penawaran
+     FROM penawarans AS subquery
+     WHERE subquery.idfk_tkd = penawarans.idfk_tkd
+     AND subquery.nilai_penawaran IS NOT NULL
+     ORDER BY subquery.nilai_penawaran DESC
+     LIMIT 1 OFFSET 3) AS nilai_penawaran4
+'),
+
+            DB::raw('
+    (SELECT nilai_penawaran
+     FROM penawarans AS subquery
+     WHERE subquery.idfk_tkd = penawarans.idfk_tkd
+     AND subquery.nilai_penawaran IS NOT NULL
+     ORDER BY subquery.nilai_penawaran DESC
+     LIMIT 1 OFFSET 4) AS nilai_penawaran5
+'),
+            DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 1)
+                 LIMIT 1) AS idfk_daftar2
+            '),
+            DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 2)
+                 LIMIT 1) AS idfk_daftar3
+            '),
+            DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 3)
+                 LIMIT 1) AS idfk_daftar4
+            '),
+            DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 4)
+                 LIMIT 1) AS idfk_daftar5
+            '),
+        )
+            ->joinSub($sub, 'subquery', function ($join) {
+                $join->on('penawarans.idfk_tkd', '=', 'subquery.idfk_tkd')
+                    ->on('penawarans.nilai_penawaran', '=', 'subquery.max_penawaran');
+            })
+            ->leftJoin('tkds', 'tkds.id', '=', 'penawarans.idfk_tkd')
+            ->leftJoin('daftars', 'daftars.id', '=', 'penawarans.idfk_daftar')
+            ->leftJoin('daftars as daftar2', 'daftar2.id', '=',  DB::raw('
+            (SELECT idfk_daftar
+             FROM penawarans AS subquery
+             WHERE subquery.idfk_tkd = tkds.id
+             AND subquery.nilai_penawaran =
+                (SELECT nilai_penawaran
+                 FROM penawarans
+                 WHERE idfk_tkd = tkds.id
+                 AND nilai_penawaran IS NOT NULL
+                 ORDER BY nilai_penawaran DESC
+                 LIMIT 1 OFFSET 1)
+             LIMIT 1)
+        '))
+            ->leftJoin('daftars as daftar3', function ($join) {
+                $join->on('daftar3.id', '=', DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 2)
+                 LIMIT 1)
+            '));
+            })
+            ->leftJoin('daftars as daftar4', function ($join) {
+                $join->on('daftar4.id', '=', DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 3)
+                 LIMIT 1)
+            '));
+            })
+            ->leftJoin('daftars as daftar5', function ($join) {
+                $join->on('daftar5.id', '=', DB::raw('
+                (SELECT idfk_daftar
+                 FROM penawarans AS subquery
+                 WHERE subquery.idfk_tkd = tkds.id
+                 AND subquery.nilai_penawaran =
+                    (SELECT nilai_penawaran
+                     FROM penawarans
+                     WHERE idfk_tkd = tkds.id
+                     AND nilai_penawaran IS NOT NULL
+                     ORDER BY nilai_penawaran DESC
+                     LIMIT 1 OFFSET 4)
+                 LIMIT 1)
+            '));
+            })
+
+            ->where('daftars.id_kelurahan', $kelurahanIdFromDaerah)
+            ->orderBy('tkds.bukti', 'DESC')
+            ->get();
+        $winners = Penawaran::select('idfk_tkd', 'idfk_daftar', 'nilai_penawaran')
+            ->whereNull('deleted_at')
+            ->where('gugur', '=', false)
+            ->orderBy('nilai_penawaran', 'DESC')
+            ->get()
+            ->groupBy('idfk_tkd')
+            ->map(function ($group) {
+                return $group->take(5);
+            });
+
+        $winnerDetails = Daftar::whereIn('id', $winners->flatten()->pluck('idfk_daftar'))->get();
+        $winnersWithDetails = $winners->map(function ($group) use ($winnerDetails) {
+            return $group->map(function ($item) use ($winnerDetails) {
+                $detail = $winnerDetails->firstWhere('id', $item->idfk_daftar);
+                $item->nama = $detail ? $detail->nama : null;
+                return $item;
+            });
+        });
         $pdf = PDF::loadview('pdf.pemenang.index', [
-            'pemenangs' => $pemenangs,
+            'penawaran' => $penawaran,
             'daerahList' => $daerahList,
-        ]);
+            'winnersWithDetails' => $winnersWithDetails,
+        ])->setPaper('a4', 'landscape');
         return $pdf->stream();
     }
 }
--- resources/views/pdf/pemenang/index.blade.php
+++ resources/views/pdf/pemenang/index.blade.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-    table{
+    table {
         border: 1px solid black;
         border-collapse: collapse;
         padding-top: 0px;
@@ -7,7 +7,8 @@
         font-size: 10px;
         font-family: "Courier";
         font-weight: bold;
-        margin: 0 auto; /* Tambahkan ini untuk mengatur margin otomatis */
+        margin: 0 auto;
+        /* Tambahkan ini untuk mengatur margin otomatis */
     }
 
     th {
@@ -18,39 +19,74 @@
         font-family: 'Arial';
         text-align: center;
     }
+
     .btbl {
         border-bottom: 2px solid black;
     }
 </style>
 
-<center style="font-family:'Arial';font-size: 15px;"><b>KONTROL 5 PEMENANG LELANG SEWA TANAH PERTANIAN {{ $daerahList->kelurahan }}</b><br>
-<br><br>
-<table>
-<thead>
-    <tr class="btbl">
-        <th rowspan="2" >No</th>
-        <th rowspan="2" >Bukti Hak</th>
-        <th rowspan="2" >Luas</th>
-        <th rowspan="2" >Harga Dasar</th>
-        <th colspan="3" >Pemenang I</th>
-        <th colspan="2" >Pemenang II</th>
-        <th colspan="2" >Pemenang III</th>
-        <th colspan="2" >Pemenang IV</th>
-        <th colspan="2" >Pemenang V</th>
-    </tr>
-    <tr>
-        <th>T.Luas</th>
-        <th>Nama</th>
-        <th>Nilai</th>
-        <th>Nama</th>
-        <th>Nilai</th>
-        <th>Nama</th>
-        <th>Nilai</th>
-        <th>Nama</th>
-        <th>Nilai</th>
-        <th>Nama</th>
-        <th>Nilai</th>
-    </tr>
-</thead>
-</table>
+<center style="font-family:'Arial';font-size: 15px;"><b>KONTROL 5 PEMENANG LELANG SEWA TANAH PERTANIAN
+        {{ $daerahList->kelurahan }}</b><br>
+    <br><br>
+    <table>
+        <thead>
+            <tr class="btbl">
+                <th rowspan="2">No</th>
+                <th rowspan="2">Bukti Hak</th>
+                <th rowspan="2">Luas</th>
+                <th rowspan="2">Harga Dasar</th>
+                <th colspan="3">Pemenang I</th>
+                <th colspan="2">Pemenang II</th>
+                <th colspan="2">Pemenang III</th>
+                <th colspan="2">Pemenang IV</th>
+                <th colspan="2">Pemenang V</th>
+                <th colspan="1">Keterangan</th>
+            </tr>
+            <tr>
+                <th>T.Luas</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+                <th>Nama</th>
+                <th>Nilai</th>
+            </tr>
+
+        </thead>
+        @foreach ($penawaran->groupBy('idfk_tkd') as $idfk_tkd => $listPenawaran)
+            @php
+                $firstPenawaran = $listPenawaran->first();
+                $winners = $winnersWithDetails[$idfk_tkd] ?? [];
+            @endphp
+            <tr>
+                <th>{{ $loop->iteration }}</th>
+                <th>{{ $firstPenawaran->bukti }}</th>
+                <th>{{ $firstPenawaran->luas }}</th>
+                <th>{{ $firstPenawaran->harga_dasar }}</th>
+                <th>{{ $firstPenawaran->total_luas }}</th>
+
+                @php
+                    $numWinners = count($winners);
+                @endphp
+
+                @foreach ($winners as $winner)
+                    <th>{{ $winner->nama }}</th>
+                    <th>{{ $winner->nilai_penawaran }}</th>
+                @endforeach
+
+                @for ($i = $numWinners; $i < 5; $i++)
+                    <th></th>
+                    <th></th>
+                @endfor
+
+                <th>{{ $firstPenawaran->keterangan }}</th>
+            </tr>
+        @endforeach
+
+
+    </table>
 </center>
