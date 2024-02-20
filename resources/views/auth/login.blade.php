99c49aadb533e777f9e84f8487769269700a31d7
Thirsya <thirsya.widya@gmail.com>
2024-02-20 13:50:34 +0700

    w

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                     <div
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <H1>Lelang Sewa</H1>
+                            <H1>Lelang Sewaaa</H1>
                         </div>
                         <div class="card card-primary">
                             <div class="card-header">
